2cd0a39cda8f0cfa2365820d548ae7ec09b7328c
luna <lunashoval@gmail.com>
2021-10-18 04:58:53 -0400

    script.js

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ first.style.cssText = 'color: red;';
 first.textContent = 'Hey, I am red';
 
 container.appendChild(first);
+
+const second = document.createElement('h3')
+second.style.cssText = 'color: blue';
+first.textContent = 'Hey, I am blue h3';
+
+container.appendChild(second);
