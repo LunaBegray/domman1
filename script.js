f121e9003e3806daa222a4cddb8b1e4251f006b4
luna <lunashoval@gmail.com>
2021-10-18 05:07:54 -0400

    script.js

--- script.js
+++ script.js
@@ -3,11 +3,17 @@ const container = document.querySelector('#container');
 const first = document.createElement('p');
 first.style.cssText = 'color: red;';
 first.textContent = 'Hey, I am red';
-
 container.appendChild(first);
 
-const second = document.createElement('h3')
+const second = document.createElement('h3');
 second.style.cssText = 'color: blue';
 first.textContent = 'Hey, I am blue h3';
-
 container.appendChild(second);
+
+const third = document.createElement('div');
+third.style.cssText = 'background-color: pink';
+third.style.cssText = 'border-color: black';
+third.setAttribute('div', 'divSmol');
+container.appendChild(third);
+
+
